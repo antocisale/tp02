f75a98bcf93cbb410ec9fe65d53a874edbb03267
Antonella Cisale <antonella.cisale@gmail.com>
2019-11-17 10:09:46 -0300

    testeado array completo con numero random

--- ventas.test.js
+++ ventas.test.js
@@ -1,23 +1,11 @@
 
 
 const localPc = require ('./ventas'),
-    obtenerIdVenta = localPc.obtenerIdVenta,
     agregarVenta = localPc.agregarVenta,
-    ventas = localPc.ventas;
+    ventas = localPc.ventas,
+    obtenerIdVenta = localPc.obtenerIdVenta;
+    
 
-const mockMath = Object.create(global.Math);
-    mockMath.random = () => 0.5;
-    localPc.obtenerIdVenta = mockMath;
-
-/*
-
-
-agregarVenta(dia, mes, anio, vendedora, sucursal, componentes): 
-recibe por parámetro todos los datos de una venta, y los agrega en el array de ventas. 
-Al igual que las ventas que ya están previamente creadas, 
-además de estos datos necesitamos agregar el primer dato que es un 
-dentificador de la venta. Para agregar este dato, tenemos que usar
-la función desarrollada en el punto anterior obtenerIdVenta*/
 beforeEach(() => {
     localPc.ventas = [];
     
@@ -30,18 +18,29 @@ test ('obtengo un ID de venta random', ()=>{
 
 });
 
-test ('agrego una venta revisando el array 0 completo', () =>{
-    agregarVenta(15,12,2018,"Ana", "Caballito", 'Monitor GPRS 3000');
-    expect (ventas [0]).toStrictEqual([
-        localPc.obtenerIdVenta(),
-            15,
-            12,
-            2018,
-            "Ana",
-            "Caballito",
-            ['Monitor GPRS 3000']
-    ]);
+describe ('pruebas en agregar venta sin numero random', ()=>{
+    beforeEach(() => {
+        jest.spyOn(global.Math, 'random').mockReturnValue(0.123456789);
+    });
+    
+    afterEach(() => {
+        global.Math.random.mockRestore();
+    });
+    test ('agrego una venta revisando el array 0 completo', () =>{
+        agregarVenta(15,12,2018,"Ana", "Caballito", 'Monitor GPRS 3000');
+        expect (ventas [0]).toStrictEqual([
+                obtenerIdVenta(),
+                15,
+                12,
+                2018,
+                "Ana",
+                "Caballito",
+                ['Monitor GPRS 3000']
+        ]);
+    }); 
 });
+
+
 
 test ('agrego una venta', () =>{
     agregarVenta(15,12,2018,"Ana", "Caballito", 'Monitor GPRS 3000');
@@ -52,5 +51,6 @@ test ('agrego una venta', () =>{
     expect (ventas [0][5]).toBe("Caballito");
     expect (ventas [0][6]).toStrictEqual(['Monitor GPRS 3000']);
 });
+
 
 
