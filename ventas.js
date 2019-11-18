3d9dd666e00e10ca311d0fa31d4b51bc2927594d
Ligia Ballesteros <ligiaballesteros@hotmail.com>
2019-11-18 14:39:48 -0300

    Funcion 2

--- ventas.js
+++ ventas.js
@@ -12,7 +12,50 @@ const vendedoras = ["Ada", "Grace", "Hedy", "Sheryl"],
     ],
     sucursales = ['Centro', 'Caballito'];
 
-let ventas = [];
+/*le cambio al ventas el let y agregue un const porque esta variadno solo su contenido. Luego hablamos */
+const ventas = [
+    [100000000, 4, 2, 2019, 'Grace', 'Centro', ['Monitor GPRS 3000', 'Motherboard ASUS 1500']],
+    [100000001, 1, 1, 2019, 'Ada', 'Centro', ['Monitor GPRS 3000', 'Motherboard ASUS 1500']],
+    [100000002, 2, 1, 2019, 'Grace', 'Caballito', ['Monitor ASC 543', 'Motherboard MZI', 'HDD Toyiva']],
+    [100000003, 10, 1, 2019, 'Ada', 'Centro', ['Monitor ASC 543', 'Motherboard ASUS 1200']],
+    [100000004, 12, 1, 2019, 'Grace', 'Caballito', ['Monitor GPRS 3000', 'Motherboard ASUS 1200']],
+    [100000005, 21, 3, 2019, 'Hedy', 'Caballito', ['Monitor ASC 543', 'Motherboard ASUS 1200', 'RAM Quinston']]
+];
+
+
+/* Funcion 1*/
+
+/* Funcion 2 */
+
+const verificarExistenciaComponente = (componente) => {
+    const existeArticulo = precios.findIndex((articulo) => {
+        return articulo[0] == componente
+    })
+    if (existeArticulo >= 0) {
+        return true
+    }
+    return false
+}
+
+const cantidadVentasComponente = (componente) => {
+    let cantidad = 0
+    if (!verificarExistenciaComponente(componente)) {
+        return "Articulo no Existe."
+    }
+    ventas.forEach(venta => {
+        const encuentra = venta[6].filter((articulo) => {
+            return articulo == componente
+        });
+        cantidad = cantidad + encuentra.length;
+    })
+    return cantidad;
+}
+
+/* Funcion 2.1 */
+
+const componenteMasVendido = ()
+
+/* Funcion 3 */
 
 const obtenerIdVenta = () => {
     let IdVenta = Math.floor(Math.random() * (1000000000 - 100000000) + 100000000);
@@ -43,5 +86,8 @@ module.exports = {
     sucursales,
     ventas,
     obtenerIdVenta,
-    agregarVenta
+    agregarVenta,
+    precioMaquina,
+    cantidadVentasComponente,
+    verificarExistenciaComponente
 };
